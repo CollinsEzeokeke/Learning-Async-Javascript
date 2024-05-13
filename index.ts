7f6efa8c2013e46dde64d222ed49bea9b0abbcab
Ezeokeke Collins Chidubem <90806850+CollinsEzeokeke@users.noreply.github.com>
2024-05-13 22:52:27 +0100

    Error handling
    i used try and catch to get a hold of errors
    
    i also got the hang of writing async functions with arrow functions and making FIFE functions(self executing functions) with arrow functions
    
    learning with practice and exploration was actually quite nice

--- index.ts
+++ index.ts
@@ -10,10 +10,58 @@ interface PokemonList {
     }[];
 }
 
+interface Pokemon {
+    id: number;
+    name: string;
+    stats: {
+        base_stat: number;
+        effort: number;
+        stat: {
+            name: string;
+            url: string;
+        };
+    }[];
+}
+
 // First method of making promises
 
-fetch('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20')
-.then(res => res.json())
-// .then((data: any) => data as PokemonList) //this is just if typescript isn't identifying my PokemonList as the type for the promise data
-.then((data: PokemonList) => console.log(data));
+// fetch('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20')
+// .then(res => res.json())
+// // .then((data: any) => data as PokemonList) //this is just if typescript isn't identifying my PokemonList as the type for the promise data
+// .then((data: PokemonList) => {
 
+//     fetch(data.results[0].url)
+// .then(res => res.json())
+// // .then((data: any) => data as PokemonList) //this is just if typescript isn't identifying my PokemonList as the type for the promise data
+// .then((data) => {
+//     console.log(data)
+// });
+// })
+// .catch((err) => {
+//     console.log(err);
+// });
+
+//second form of making promises using await 
+
+const getPokemonList = async (): Promise<PokemonList> => {
+    const listResp = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20");
+    return await listResp.json();    
+}
+
+const getPokemon = async (url: string): Promise<Pokemon> => {
+    const dataResp = await fetch(url);
+    return await dataResp.json();
+}
+
+
+(async () => {
+    try{
+    // const listResponse = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=20');
+    // const list: PokemonList = await listResponse.json();
+    const list = await getPokemonList();
+    const pokemon = await getPokemon(list.results[0].url)
+        console.log(pokemon.name);
+    } catch (e) {
+        console.log(e) 
+    }//for errors
+})();
